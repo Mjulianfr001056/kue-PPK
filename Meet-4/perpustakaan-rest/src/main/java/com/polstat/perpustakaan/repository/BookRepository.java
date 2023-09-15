330e19e0d9b6f07c57546bee0baf965c5051ab6d
Mjulianfr001056 <222112217@stis.ac.id>
2023-09-15 21:41:32 +0700

    Meet-4 Update

---
+++ Meet-4/perpustakaan-rest/src/main/java/com/polstat/perpustakaan/repository/BookRepository.java
@@ -0,0 +1,5 @@
+package com.polstat.perpustakaan.repository;
+import com.polstat.perpustakaan.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface BookRepository extends JpaRepository<Book, Long>{
+}
